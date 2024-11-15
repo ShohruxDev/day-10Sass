30a076d9fed5a65f241f1dd79908f8c0e8d74e60
Timurbek <timurbeksaburovv.@gmail.com>
2024-11-15 05:16:59 +0200

    qo'shimcha qo'shildi

--- style/script.js
+++ style/script.js
@@ -23,15 +23,15 @@ backToTopButton.addEventListener("click", scrollToTop);
 
 
 const menu = document.querySelector(".menu");
-  const menuMobile = document.querySelector(".box-3");
-  const close = document.querySelector(".close");
+const menuMobile = document.querySelector(".menu-mobile");
+const close = document.querySelector(".close");
 
-  menu.addEventListener("click", () => {
-    menuMobile.classList.add("active");
-    document.body.style.overflow = "hidden";
-  });
+menu.addEventListener("click", () => {
+  menuMobile.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
 
-  close.addEventListener("click", () => {
-    menuMobile.classList.remove("active");
-    document.body.style.overflowY = "auto";
-  });
+close.addEventListener("click", () => {
+  menuMobile.classList.remove("active");
+  document.body.style.overflowY = "auto";
+});
